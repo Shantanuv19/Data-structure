bc1fc2ad14ef9ed4a545fa50f973bbfa5d2afb22
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-01-12 13:00:15 +0530

    Add files via upload

---
+++ oops with C++/even_or_odd(14).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a==0)
+    cout<<"odd"<<endl;
+    else
+    cout<<"even";
+}
+
