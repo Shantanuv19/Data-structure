d6806123b9e31be06c3f0fe3c753ffbb6b3adbe6
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-01-12 12:41:24 +0530

    Add files via upload

---
+++ oops with C++/10float.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    float y;
+    cin>>y;
+    cout<<x<<endl;
+    cout<<y<<endl;
+}
+
