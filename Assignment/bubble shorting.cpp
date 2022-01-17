7f2878d46485441cd50d4acae51a01b527dee1f8
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-01-17 11:35:55 +0530

    Update bubble shorting.cpp

--- Assignment/bubble shorting.cpp
+++ Assignment/bubble shorting.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main ()
 {
-   int a, j, temp, arr[max], ele, size;
+   int a, j, x, arr[max], ele, size;
    cout<<"Enter the size of the arr:- ";
    cin>>size;
    for(a=0; a<size; a++)
@@ -19,15 +19,15 @@ cout<<endl;
 for(a=0; a<size; a++) {
    for(j=a+1; j<size; j++)
    {
-      if(arr[j] < arr[a]) {
-         temp = arr[a];
+      while(arr[j] < arr[a]) {
+         x = arr[a];
          arr[a] = arr[j];
-         arr[j] = temp;
+         arr[j] = x;
       }
    }
 }
 cout <<"after shorting the element:- \n";
-for(a=0; a<size; a++) {
+for(a=0; a<size; a++){
    cout <<arr[a]<<"\t";
 }
 }
