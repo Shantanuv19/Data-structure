b979d5840f4fba602e3c6c9aa4952462dc7e1310
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-22 01:31:49 +0530

    Update 7 value_copy.cpp

--- Array using C++/7 value_copy.cpp
+++ Array using C++/7 value_copy.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
-#define max_size 1000
 using namespace std;
 int main()
 {
-	int a[max_size], b[max_size];
-	int d,c;
-	cout<<"Enter the size of the arr;- ";
-	cin>>d;
-	for(c=1; c<=d; c++)
+	int size, a, b, c,ne=0;
+	cout<<"Enter the size of the arr:- ";
+	cin>>size;
+	int arr1[size], arr[100];
+	for(a=0; a<size; a++)
 	{
-		cout<<"Enter the element of the arr:- ";
-		cin>>a[c];
+		cin>>arr[a];
+		arr1[a]=arr[a];
 	}
-	
-	for(c=1; c<=d; c++)
+	a=0;
+	do
 	{
-		b[c]=a[c];
-	}
-	for(c=1; c<=d; c++)
-	{
-		cout<<"\nValue of the b[size] is:- "<<b[c];
-	}
+		cout<<arr1[a]<<ends;
+		a++;
+	}while(a!=size);
 }
